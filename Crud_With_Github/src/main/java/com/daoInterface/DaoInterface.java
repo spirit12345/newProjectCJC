b159e9a342dd021737501070068422a032197304
vaishnaviyerawar <vyerawar13@gmail.com>
2018-10-25 09:58:44 +0530

    changes done

--- Crud_With_Github/src/main/java/com/daoInterface/DaoInterface.java
+++
@@ -1,9 +0,0 @@
-package com.daoInterface;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.model.Login;
-
-public interface DaoInterface extends CrudRepository<Login, Integer>{
-
-}
--- Crud_With_Github/src/main/java/serviceInterface/serviceInterface.java
+++
@@ -1,5 +0,0 @@
-package serviceInterface;
-
-public interface serviceInterface {
-
-}
